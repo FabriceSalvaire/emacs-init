6514cbb33dc1ec25fe0b5c38a663b95c2ae69e1d
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2024-02-06 01:15:10 +0100

    emacs

--- emacs/dot-emacs.el
+++ emacs/dot-emacs.el
@@ -1,8 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                                                                                  ;
-;;                                              GNU-Emacs                                           ;
-;;                                                                                                  ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                                                                  
+;; Top Emacs Configuration
+;;
 
 ;; M-x load-file ~./emacs
 ;;
@@ -14,48 +13,51 @@
 ;; Path Settings
 ;;
 
-(setq local_path_prefix "/home/common/emacs/" ; this is the only absolut path for Emacs files
-      local_emacs_site_lisp_path (concat local_path_prefix "emacs-site-lisp/")
-      local_emacs_d_path (concat local_path_prefix "emacs.d/")
-      )
+(defconst local_path_prefix "/home/common/emacs/") ; this is the only absolut path for Emacs files
+(defconst local_emacs_site_lisp_path (concat local_path_prefix "emacs-site-lisp/"))
+(defconst local_emacs_d_path (concat local_path_prefix "emacs.d/"))
 
 (add-to-list 'load-path local_emacs_d_path)
 (add-to-list 'load-path local_emacs_site_lisp_path)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(load "packages.el" t t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; Load sub-config files
 ;;
 
-(load "variables.el" t t)
-(load "user-functions.el" t t)
+;; (load FILE &optional NOERROR NOMESSAGE NOSUFFIX MUST-SUFFIX)
+(mapcar (lambda (_) (load (concat _ ".el") t t))
+	'(
+	  "packages"
 
-(load "frame.el" t t)
-(load "font.el" t t)
-(load "behaviour.el" t t)
-(load "encoding.el" t t)
-(load "treemacs-settings.el" t t)
-(load "completion.el" t t) ; buffer switch
-(load "edition-settings.el" t t)
+	  "variables"
+	  "user-functions"
 
-(load "modes.el" t t)
-(load "sysadmin.el" t t)
+	  "frame"
+	  "font"
+	  "behaviour"
+	  "encoding"
+	  "treemacs-settings"
+	  "completion" ; buffer switch
+	  "edition-settings"
 
-(load "programming.el" t t)
-(load "mode-languages.el" t t)
-(load "mode-c.el" t t)
-(load "mode-python.el" t t)
-(load "mode-web.el" t t)
+	  "modes"
+	  "sysadmin"
 
-(load "spelling.el" t t)
-(load "flycheck-settings.el" t t)
+	  "programming"
+	  "mode-languages"
+	  "mode-c"
+	  "mode-python"
+	  "mode-web"
 
-;; must be after
-(load "keys.el" t t)
+	  "spelling"
+	  "flycheck-settings"
+
+	  ;; must be after
+	  "keys"
+
+	  "server-settings"
+	  ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -64,28 +66,3 @@
 
 ;; (setq custom-file (concat local_path_prefix "gnu-emacs-custom.el"))
 ;; (load custom-file t t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Load post-config files
-;;
-
-(load "server-settings.el" t t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(package-selected-packages
-   '(zettelkasten typescript-mode astro-ts-mode xterm-color poly-markdown numpydoc python-docstring python-insert-docstring sphinx-doc flycheck-eglot impatient-showdown multiple-cursors treesit-auto eglot corfu zzz-to-char python-mode all-the-icons-ivy anything apache-mode auctex-lua auto-complete auto-complete-c-headers cff clang-format clean-aindent-mode cmake-font-lock cmake-ide cmake-mode cmake-project column-marker company-anaconda company-auctex company-box company-c-headers company-irony company-irony-c-headers company-jedi company-math company-qml company-shell cpputils-cmake csharp-mode cuda-mode dap-mode django-mode docker-compose-mode dockerfile-mode doom-modeline dotenv-mode flycheck-color-mode-line flycheck-grammalecte flycheck-irony flycheck-kotlin flycheck-pos-tip flycheck-pycheckers flycheck-tip flymake-shell flyspell-correct-ivy flyspell-lazy glsl-mode go-mode google-translate gradle-mode graphql-mode graphviz-dot-mode haskell-mode highlight-indentation icicles icomplete+ importmagic indent-guide irony-eldoc jinja2-mode json-mode kotlin-mode language-detection languagetool live-py-mode magit markdown-mode markdown-mode+ markdown-preview-mode markdown-toc modern-cpp-font-lock php-mode pippel powerline projectile projectile-ripgrep projectile-speedbar protobuf-mode pyimport pylint python-django pyvenv qml-mode react-snippets rjsx-mode sass-mode scss-mode smartparens spice-mode string-inflection systemd tern treemacs-all-the-icons treemacs-icons-dired treemacs-magit treemacs-perspective treemacs-projectile treemacs-tab-bar typoscript-mode vue-mode which-key))
- '(python-indent-def-block-scale 1)
- '(tool-bar-mode nil)
- '(typescript-ts-mode-indent-offset 4))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Noto Sans Mono" :foundry "GOOG" :slant normal :weight normal :height 143 :width normal)))))
