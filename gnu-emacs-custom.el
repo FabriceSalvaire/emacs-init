99683980526f1d3cdbeb80d49deb12cbbcb91b4e
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2024-02-08 23:01:12 +0100

    clean custom

--- gnu-emacs-custom.el
+++ gnu-emacs-custom.el
@@ -3,22 +3,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(helpful gcmh git-gutter all-the-icons-ivy anything apache-mode auctex-lua auto-complete auto-complete-c-headers cff clang-format clean-aindent-mode cmake-font-lock cmake-ide cmake-mode cmake-project column-marker company-anaconda company-auctex company-box company-c-headers company-irony company-irony-c-headers company-jedi company-math company-qml company-shell cpputils-cmake csharp-mode cuda-mode dap-mode django-mode docker-compose-mode dockerfile-mode doom-modeline dotenv-mode flycheck-color-mode-line flycheck-grammalecte flycheck-irony flycheck-kotlin flycheck-pos-tip flycheck-pycheckers flycheck-tip flymake-shell flyspell-correct-ivy flyspell-lazy glsl-mode go-mode google-translate gradle-mode graphql-mode graphviz-dot-mode haskell-mode highlight-indentation icicles icomplete+ importmagic indent-guide irony-eldoc jinja2-mode json-mode kotlin-mode language-detection languagetool live-py-mode magit markdown-mode markdown-mode+ markdown-preview-mode markdown-toc modern-cpp-font-lock php-mode pippel powerline projectile projectile-ripgrep projectile-speedbar protobuf-mode pyimport pylint python-django pyvenv qml-mode react-snippets rjsx-mode sass-mode scss-mode smartparens spice-mode string-inflection systemd tern treemacs-all-the-icons treemacs-icons-dired treemacs-magit treemacs-perspective treemacs-projectile treemacs-tab-bar typoscript-mode vue-mode which-key)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(diredfl-compressed-file-name ((t (:foreground "tomato"))))
- '(diredfl-compressed-file-suffix ((t (:foreground "light salmon"))))
- '(diredfl-date-time ((t (:foreground "cornflower blue"))))
- '(diredfl-deletion ((t (:background "tomato" :foreground "black"))))
- '(diredfl-deletion-file-name ((t (:foreground "orange red"))))
- '(diredfl-dir-name ((t (:background "gray20" :foreground "white smoke" :weight semi-bold))))
- '(diredfl-file-name ((t (:foreground "gainsboro"))))
- '(diredfl-file-suffix ((t (:foreground "peru"))))
- '(diredfl-number ((t (:foreground "pale goldenrod"))))
- '(whitespace-empty ((t (:background "#999999999999" :foreground "#ee0000"))))
- '(whitespace-tab ((t (:background "#999999999999" :foreground "#ee0000"))))
- '(whitespace-trailing ((t (:background "#999999999999" :foreground "#ee0000")))))
+)
