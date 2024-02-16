84fa449a1775288779c6989b56819c3b419f3915
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2024-02-16 19:26:27 +0100

    fix startup

--- lisp/core/bootstrap.el
+++ lisp/core/bootstrap.el
@@ -4,29 +4,52 @@
 ;;; Code:
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Hooks
 
-(defvar init--module-options nil)
+(defcustom doom-before-modules-init-hook nil
+  "Hooks run before module init.el files are loaded."
+  :group 'doom
+  :type 'hook)
 
-(defun init--load-modules (args)
-  "Helper to load Lisp files."
-  (catch ':stop
-    (dolist (module args)
-      (if (eq module ':stop)
-          (progn
-            (message "Stop sub-module loading")
-            (throw ':stop nil))
-        (let ((fist-char (substring (symbol-name module) 0 1)))
-          (if (equal fist-char "+")
-              (push  module init--module-options)
-            (let ((path (concat (symbol-name module) ".el")))
-              ;; (load FILE &optional NOERROR NOMESSAGE NOSUFFIX MUST-SUFFIX)
-              ;; (load path t t)
-              (load path nil nil)
-              )))))))
+(defcustom doom-after-modules-init-hook nil
+  "Hooks run after module init.el files are loaded."
+  :group 'doom
+  :type 'hook)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(init--load-modules '(
+(load "doom-lib/doom-lib.el")
+
+(defvar init--module-options nil)
+
+;; Fixme: load doom-libe befora and merge
+(defun init--load-modules-private (args)
+  "Helper to load Lisp files."
+  (progn
+    (catch ':stop
+      (dolist (module args)
+        (if (eq module ':stop)
+            (progn
+              (message "Stop sub-module loading")
+              (throw ':stop nil))
+          (let ((fist-char (substring (symbol-name module) 0 1)))
+            (if (equal fist-char "+")
+                (push  module init--module-options)
+              (let ((path (concat (symbol-name module) ".el"))
+                    (load-time (float-time)))
+                ;; (load FILE &optional NOERROR NOMESSAGE NOSUFFIX MUST-SUFFIX)
+                ;; (load path t t)
+                (load path nil nil)
+                (doom-log "loaded '%s' (%.1f us)"
+                          path
+                          (* (- (float-time) load-time) 1000))
+                ))))))
+    ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(init--load-modules-private '(
                ;; instead to comment several lines to stop at some point
                ;; use keyword
                ;;   :stop
@@ -36,7 +59,7 @@
 
                ;; Fixme: move
                ;; load internal libraries
-               doom-lib/doom-lib
+               ;; doom-lib/doom-lib ; loaded before
                doom-lib/buffers
                doom-lib/plist
                doom-lib/ui
@@ -56,5 +79,14 @@
                ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun init--load-modules (args)
+  "Helper to load Lisp files."
+  (progn
+    (doom-run-hooks 'doom-before-modules-init-hook)
+    (init--load-modules-private args)
+    ;; see doom-emacs/lisp/doom-profiles.el
+    (doom-run-hooks 'doom-after-modules-init-hook)
+    ))
 
 ;;; bootstrap.el ends here
